80e2654a1c7447f43bcc8c6443ab1c11b8b81641
unknown <narminna@code.edu.az>
2023-07-10 16:40:45 +0400

    Reversed&UpperCased

---
+++ Reverse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input:");
+            string input = Console.ReadLine();
+
+            string reversedString = test(input);
+            Console.WriteLine($"Output: {reversedString}");
+        }
+
+        public static string test(string input)
+        {
+            return new string(input.ToCharArray().Reverse().ToArray()).ToUpper();
+        }
+    }
+}
